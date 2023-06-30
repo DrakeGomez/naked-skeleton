2a736ce0bc50216ddbee669779548995dcf79a1f
DrakeGomez <110204427+DrakeGomez@users.noreply.github.com>
2023-06-30 17:32:43 -0400

    Add files via upload
    Added functions.php file.

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * This file adds functions to the Naked Skeleton block theme for WordPress.
+ */
+
+ //Require patterns.php file
+ require get_template_directory() . '/inc/patterns.php';
